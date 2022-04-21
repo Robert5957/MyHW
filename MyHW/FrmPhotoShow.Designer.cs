edbebd1cd5588977c5f10e126ef4f8440f4763ca
Robert5957 <charles.chou570114@gmail.com>
2022-04-22 00:52:37 +0800

    add the filter of file to upload image by folder

---
+++ MyHW/FrmPhotoShow.Designer.cs
@@ -0,0 +1,50 @@
+﻿
+namespace MyHW
+{
+    partial class FrmPhotoShow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmPhotoShow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 450);
+            this.Name = "FrmPhotoShow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "FrmPhotoShow";
+            this.Load += new System.EventHandler(this.FrmPhotoShow_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.FrmPhotoShow_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ MyHW/FrmPhotoShow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyHW
+{
+    public partial class FrmPhotoShow : Form
+    {
+        public FrmPhotoShow()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmPhotoShow_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmPhotoShow_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
